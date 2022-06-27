1acf00deb76e74094c8335e070797eb62c925d89
walumo <miikka.hakulinen@awacademy.training>
2022-06-27 13:41:07 +0300

    IsLate method added

--- Checker.cs
+++ Checker.cs
@@ -11,5 +11,20 @@ namespace DateChecker
             else
                 return false;
         }
+
+        public static bool IsLate(DateTime startLearningDate, decimal hoursToComplete)
+        {
+            if (startLearningDate.AddHours(Convert.ToDouble(hoursToComplete)).CompareTo(DateTime.Now) >= 0)
+                return true;
+            else
+                return false;
+        }
+        public static bool IsLate(DateTime startLearningDate, double hoursToComplete)
+        {
+            if (startLearningDate.AddHours(hoursToComplete).CompareTo(DateTime.Now) >= 0)
+                return true;
+            else
+                return false;
+        }
     }
 }
